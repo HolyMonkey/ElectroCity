140aeb7fa4cb5e13a4ded27df0499da329e38b2e
PlotvaIzLodzya <86444967+PlotvaIzLodzya@users.noreply.github.com>
2022-07-13 21:12:23 +0300

    ropeAttach

--- Assets/Scripts/Rope/RopePickUpTrigger.cs
+++ Assets/Scripts/Rope/RopePickUpTrigger.cs
@@ -5,12 +5,14 @@ public class RopePickUpTrigger : MonoBehaviour
 {
     [SerializeField] private ObiParticleAttachment _attachment;
     [SerializeField] private ObiRope _rope;
+    [SerializeField] private Transform _ropeEnd;
 
     private void OnTriggerEnter(Collider other)
     {
         if(other.TryGetComponent(out Player player))
         {
-            _attachment.target = player.RopePoint.transform;
+            _ropeEnd.SetParent(player.RopePoint.transform);
+            _ropeEnd.localPosition = Vector3.zero;
 
             //_rope.path.AddControlPoint(player.RopePoint.position, player.RopePoint.position, player.RopePoint.position,
             //    player.RopePoint.position, 0.1f, 0.1f, 1f, 0, Color.white, "point");
