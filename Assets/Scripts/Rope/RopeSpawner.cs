f99c3213637e6afd20ac809d50fbe7f14154f5ca
humbleGuy1 <vrudnevskiy@list.ru>
2022-07-15 19:47:38 +0300

    NewRope

--- Assets/Scripts/Rope/RopeSpawner.cs
+++ Assets/Scripts/Rope/RopeSpawner.cs
@@ -11,15 +11,30 @@ public class RopeSpawner : MonoBehaviour
 
     public void Spawn(Player player)
     {
-        _solver.transform.position = player.transform.position;
+        _solver.transform.position = _startPoint.transform.position;
         var rope = Instantiate(_ropePrefab, _solver.transform);
 
         rope.StartPoint.SetParent(_startPoint);
-        rope.StartPoint.localPosition = Vector3.zero;
-
         rope.EndPoint.SetParent(player.RopePoint);
-        rope.EndPoint.localPosition = Vector3.zero;
+        
+        StartCoroutine(Attaching(rope));
 
         player.TakeRope(rope);
+    }
+
+    private IEnumerator Attaching(Rope rope)
+    {
+        float runningTime = 0;
+        float moveTime = 0.1f;
+
+        while (rope.StartPoint.localPosition != Vector3.zero && rope.EndPoint.localPosition != Vector3.zero)
+        {
+            runningTime += Time.deltaTime;
+            rope.StartPoint.localPosition = Vector3.Lerp(rope.StartPoint.localPosition, Vector3.zero, runningTime/moveTime);
+            rope.EndPoint.localPosition = Vector3.Lerp(rope.EndPoint.localPosition, Vector3.zero, runningTime / moveTime);
+
+            yield return null;
+        }
+
     }
 }
