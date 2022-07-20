31add295b634d9671dd18a1ca4a4605bb8572911
PlotvaIzLodzya <86444967+PlotvaIzLodzya@users.noreply.github.com>
2022-07-21 00:02:33 +0300

    fixes

--- Assets/Scripts/Rope/SetRopeTrigger.cs
+++ Assets/Scripts/Rope/SetRopeTrigger.cs
@@ -10,10 +10,14 @@ public class SetRopeTrigger : MonoBehaviour
     private Team _team;
     private int _numberOfPlacements;
     private readonly int _counter = 1;
+    private int _settedRopeCounter;
+    private float _startAngle = 90f;
 
     public bool IsTryingToPlaceTwice => _numberOfPlacements < _counter;
     //public bool IsConnected => _building.IsConnected;
     public TeamId TeamId => _building.TeamId;
+
+
 
     private void OnTriggerEnter(Collider other)
     {
@@ -47,11 +51,23 @@ public class SetRopeTrigger : MonoBehaviour
         _team = handler.Team;
         _building.AddSetedRope(handler.CurrentRope);
         _building.TryCapture(_team, handler.CurrentRope);
-        handler.PlaceRope(_connectPoint);
+        handler.PlaceRope(CreateNextAttachPoint());
     }
 
     private bool CanAttach(Collider other, out RopeHandler handler)
     {
         return other.TryGetComponent(out handler) && handler.HasRope && !IsTryingPlaceTwice(handler.Team.TeamId) && handler.PickUpTrigger.Building != _building;
+    }
+
+    private Transform CreateNextAttachPoint()
+    {
+        GameObject newAttachPoint = Instantiate(new GameObject(), _connectPoint);
+        _settedRopeCounter++;
+        float angleStep = 180f / 6;
+        float angle = _startAngle - angleStep * _settedRopeCounter;
+        newAttachPoint.transform.localRotation *= Quaternion.Euler(0, angle, 0);
+        newAttachPoint.transform.localPosition += newAttachPoint.transform.forward * _connectPoint.GetComponent<SphereCollider>().radius;
+
+        return newAttachPoint.transform;
     }
 }
