082744f745254fb422fc9b4de41b04d4edc8f9f0
humbleGuy1 <vrudnevskiy@list.ru>
2022-07-25 16:19:15 +0300

    Visual

---
+++ Assets/Scripts/Building/BasciZoneColorChanger.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasciZoneColorChanger : MonoBehaviour
+{
+    [SerializeField] private Building _building;
+    [SerializeField] private ParticleSystem _zone;
+
+    [System.Obsolete]
+    private void OnEnable()
+    {
+        _building.CapturingSystem.TeamChanged += ChangeColor;
+
+    }
+
+    [System.Obsolete]
+    private void OnDisable()
+    {
+        _building.CapturingSystem.TeamChanged -= ChangeColor;
+    }
+
+    [System.Obsolete]
+    private void ChangeColor(Team team)
+    {
+        _zone.startColor = team.Color;
+    }
+}
