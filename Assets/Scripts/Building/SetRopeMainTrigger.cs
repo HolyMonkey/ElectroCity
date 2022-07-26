41b52b658a220d8f401d124251d114daa791d93d
humbleGuy1 <vrudnevskiy@list.ru>
2022-07-26 16:44:33 +0300

    ConnectionFixes

--- Assets/Scripts/Building/SetRopeMainTrigger.cs
+++ Assets/Scripts/Building/SetRopeMainTrigger.cs
@@ -5,48 +5,33 @@ public class SetRopeMainTrigger : MonoBehaviour
 {
     [SerializeField] private List<SetRopeTrigger> _triggers;
 
-    private bool _isFound;
-
     private void OnTriggerEnter(Collider other)
     {
         if(other.TryGetComponent(out RopeHandler handler))
         {
-            int counter = 0;
-
-            while (_isFound == false && counter < _triggers.Count)
-            {
-                FindClosestTrigger(handler, other, counter);
-                counter++;
-            }
+            FindClosestTrigger(handler, other);
         }
     }
 
-    private void OnTriggerExit(Collider other)
+    private void FindClosestTrigger(RopeHandler handler, Collider other)
     {
-        if (other.TryGetComponent(out RopeHandler _))
-        {
-            _isFound = false;
-        }
-    }
-
-    private void FindClosestTrigger(RopeHandler handler, Collider other, int value)
-    {
-        float minDistance = Mathf.Infinity;
+        float distance = Mathf.Infinity;
         SetRopeTrigger closestTrigger = null;
 
-        for (int i = 0; i < _triggers.Count - value; i++)
+        for (int i = 0; i < _triggers.Count; i++)
         {
-            if (minDistance > Vector3.Distance(handler.transform.position, _triggers[i].transform.position))
+            if (distance > Vector3.Distance(handler.transform.position, _triggers[i].transform.position))
             {
-                minDistance = Vector3.Distance(handler.transform.position, _triggers[i].transform.position);
+                distance = Vector3.Distance(handler.transform.position, _triggers[i].transform.position);
                 closestTrigger = _triggers[i];
+                print(closestTrigger.CanAttach(other, out handler));
             }
         }
 
         if (closestTrigger != null && closestTrigger.CanAttach(other, out handler))
         {
             closestTrigger.Attach(handler);
-            _isFound = true;
+            _triggers.Remove(closestTrigger);
         }
     }
 }
