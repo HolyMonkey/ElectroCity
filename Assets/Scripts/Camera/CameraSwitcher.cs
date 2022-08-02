04f3b797e56c2b24b969a07abcf52bf7694fef2c
humbleGuy1 <vrudnevskiy@list.ru>
2022-08-02 13:49:57 +0300

    Cameras

---
+++ Assets/Scripts/Camera/CameraSwitcher.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using Cinemachine;
+using UnityEngine;
+
+public class CameraSwitcher : MonoBehaviour
+{
+	[SerializeField] private CinemachineVirtualCamera _playerCamera;
+	[SerializeField] private CinemachineVirtualCamera _topViewCamera;
+    [SerializeField] private float _delay;
+
+    private void Start()
+    {
+        StartCoroutine(Changing());
+    }
+
+    private IEnumerator Changing()
+    {
+        yield return new WaitForSeconds(_delay);
+
+        _topViewCamera.Priority = -1;
+    }
+}
