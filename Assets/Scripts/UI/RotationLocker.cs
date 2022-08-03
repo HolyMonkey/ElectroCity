eefce9765ac54b99a42ab6df97142bda66a33120
PlotvaIzLodzya <86444967+PlotvaIzLodzya@users.noreply.github.com>
2022-08-03 13:50:11 +0300

    Update RotationLocker.cs

--- Assets/Scripts/UI/RotationLocker.cs
+++ Assets/Scripts/UI/RotationLocker.cs
@@ -6,5 +6,6 @@ public class RotationLocker : MonoBehaviour
     private void Update()
     {
         transform.LookAt(Camera.main.transform.position);
+        transform.eulerAngles = new Vector3(transform.eulerAngles.x, transform.eulerAngles.y, 0f);
     }
 }
